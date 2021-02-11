a27b5121dbbae1204ab5873f615fee57ac9dbd6f
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2021-02-11 09:36:35 +0700

    add sliders table

---
+++ app/Http/Controllers/API/v1/SliderController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Slider;
+use Illuminate\Http\Request;
+
+class SliderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $res = Slider::get();
+        return response()->json($res);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Slider $slider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Slider $slider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Slider  $slider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Slider $slider)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\v1\ProductCategoryController;
+use App\Http\Controllers\API\v1\ProductController;
+use App\Http\Controllers\API\v1\SettingController;
+use App\Http\Controllers\API\v1\SliderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\v1\ProductController;
-use App\Http\Controllers\API\v1\ProductCategoryController;
-use App\Http\Controllers\API\v1\SettingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +16,15 @@ use App\Http\Controllers\API\v1\SettingController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::apiResources([
-    'product'=>ProductController::class,
-    'productcategory'=>ProductCategoryController::class,
-    'setting'=>SettingController::class
+    'product' => ProductController::class,
+    'productcategory' => ProductCategoryController::class,
+    'setting' => SettingController::class,
+    'slider' => SliderController::class,
 ]);
